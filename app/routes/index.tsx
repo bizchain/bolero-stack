281c9a505fd69d9f87f3d5b0574dafd6f8b64e5d
Brian Cao <chasoft@users.noreply.github.com>
2022-05-26 17:22:20 +0700

    - eslint config - format all files according new formatter

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,15 @@
 import * as React from "react"
 
 export default function Index() {
-  return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold underline">
+	return (
+		<div className="p-10">
+			<h1 className="text-3xl font-bold underline">
         Hello world!
-      </h1>
-      <div className="prose">
-        <p>hello world</p>
-        <blockquote>yes please</blockquote>
-      </div>
-    </div>
-  );
+			</h1>
+			<div className="prose">
+				<p>hello world</p>
+				<blockquote>yes please</blockquote>
+			</div>
+		</div>
+	)
 }
