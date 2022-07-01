c7e6f4e849cf2bc35782430ba640a74374c7f543
Brian Cao <chasoft@users.noreply.github.com>
2022-07-01 17:34:56 +0700

    update tracking header

--- app/utils/useErrorReport.ts
+++ app/utils/useErrorReport.ts
@@ -1,3 +1,23 @@
+/*************************************************************************
+ * ╔═══════════════════════════════════════════════════════════════════╗ *
+ * ║   useErrorReport  | 1.0.0                                         ║ *
+ * ╠═══════════════════════════════════════════════════════════════════╣ *
+ * ║                                                                   ║ *
+ * ║   @author     A. Cao <cao@anh.pw>                                 ║ *
+ * ║   @copyright  Chasoft Labs © 2022                                 ║ *
+ * ║   @link       https://chasoft.net                                 ║ *
+ * ║                                                                   ║ *
+ * ╟───────────────────────────────────────────────────────────────────╢ *
+ * ║ @license This product is licensed and sold at CodeCanyon.net      ║ *
+ * ║ If you have downloaded this from another site or received it from ║ *
+ * ║ someone else than me, then you are engaged in an illegal activity.║ *
+ * ║ You must delete this software immediately or buy a proper license ║ *
+ * ║ from http://codecanyon.net/user/chasoft/portfolio?ref=chasoft.    ║ *
+ * ╟───────────────────────────────────────────────────────────────────╢ *
+ * ║      THANK YOU AND DON'T HESITATE TO CONTACT ME FOR ANYTHING      ║ *
+ * ╚═══════════════════════════════════════════════════════════════════╝ *
+ ************************************************************************/
+
 import * as React from "react"
 
 import { useFetcher } from "@remix-run/react"
@@ -7,20 +27,21 @@ import dayjs from "dayjs"
 import { useMatchesById } from "./useMatchesById"
 import { TRootDataLoader } from "~/root"
 
-export default function useErrorReport(message: string, pathname: string){
+export default function useErrorReport(message: string, url: string){
 	const errorHandle = useFetcher()
 	const rootData = useMatchesById("root").data as TRootDataLoader
 
 	React.useEffect(()=>{
 		errorHandle.submit(
 			{
-				//secret is hard-coded, DO NOT CHANGE
+				//DO NOT MODIFY THIS VALUE
+				//This used to protect this error report api endpoint
 				secret: "ec6h%T^9dvnt^CL5aY",
 				error: message,
 				date: dayjs().format(DATE_FORMAT.FULL),
 				email: rootData?.user?.email ?? "",
 				name: rootData?.user?.Name ?? "",
-				pathname
+				url
 			},
 			{ method: "post", action: "/api/error-report" }
 		)
--- app/utils/useMatchesById.ts
+++ app/utils/useMatchesById.ts
@@ -1,3 +1,23 @@
+/*************************************************************************
+ * ╔═══════════════════════════════════════════════════════════════════╗ *
+ * ║   useMatchesById  | 1.0.0                                         ║ *
+ * ╠═══════════════════════════════════════════════════════════════════╣ *
+ * ║                                                                   ║ *
+ * ║   @author     A. Cao <cao@anh.pw>                                 ║ *
+ * ║   @copyright  Chasoft Labs © 2022                                 ║ *
+ * ║   @link       https://chasoft.net                                 ║ *
+ * ║                                                                   ║ *
+ * ╟───────────────────────────────────────────────────────────────────╢ *
+ * ║ @license This product is licensed and sold at CodeCanyon.net      ║ *
+ * ║ If you have downloaded this from another site or received it from ║ *
+ * ║ someone else than me, then you are engaged in an illegal activity.║ *
+ * ║ You must delete this software immediately or buy a proper license ║ *
+ * ║ from http://codecanyon.net/user/chasoft/portfolio?ref=chasoft.    ║ *
+ * ╟───────────────────────────────────────────────────────────────────╢ *
+ * ║      THANK YOU AND DON'T HESITATE TO CONTACT ME FOR ANYTHING      ║ *
+ * ╚═══════════════════════════════════════════════════════════════════╝ *
+ ************************************************************************/
+
 import * as React from "react"
 import { useMatches } from "@remix-run/react"
 
